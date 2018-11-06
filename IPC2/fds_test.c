7f3904a637e5a8a624dd00ef58539c2afbb6d949
mint <3432747@mail.ru>
2018-11-06 22:48:43 +0300

    fds_test.c finished (check 2 file descriptors as they are pointing to the same file)

--- IPC2/fds_test.c
+++ IPC2/fds_test.c
@@ -2,30 +2,100 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
 
+#include <sys/vfs.h>
+#include <sys/statvfs.h>
+#include <inttypes.h>
+
+#define TRUE  1
+#define FALSE 0
+
+/*
+	Pair "inode + filesystem_id" determines file uniquely. 
+
+	open(), close(), read(), write(), link(), unlink()
+	operations on a file descriptor do not change its inode.
+
+	rename() can be divided into 2 situations:
+	1. file stays in the same filesystem
+	2. file crosses boundaries of its filesystem
+
+	1. rename() doesn't change inode, only names and dentries are
+	affected. So it is enougth to make sure that files have same 
+	inodes and the are in the same fs (files may have same inodes
+	in different fs)
+
+	2. rename() crosses boundaries of the current fs. In this case
+	unlink() is called and we treat files as different as writing
+	and reading from those file decriptors will write and read 
+	different blocks on a disk. 
+ */
+int TestFDs(int fd1, int fd2);
+
 int main(int argc, char const *argv[])
 {
-	// 
+	if (argc != 3)
+	{
+		printf("Expected 3 argumants\n");
+		return EXIT_FAILURE;
+	}
+
+	int fd1 = open(argv[1], O_RDWR);
+	if (fd1 == -1)
+	{
+		perror("open(), fd1");
+		return EXIT_FAILURE;
+	}
+	int fd2 = open(argv[2], O_RDWR);
+	if (fd2 == -1)
+	{
+		perror("open(), fd2");
+		return EXIT_FAILURE;
+	}
+
+	printf("res = %d\n", TestFDs(fd1, fd2));
 	return 0;
 }
 
 int TestFDs(int fd1, int fd2)
 {
-	struct stat fd1_stat = {};
-	if ( (fstat(fd1, &fd1_stat)) == -1)
+	struct stat fd_stat[2] = {};
+	if ( (fstat(fd1, &fd_stat[0])) == -1)
 	{
-		perror("stat for fd1");
-		return -1;
+		perror("fstat() for fd1");
+		return -EXIT_FAILURE;
+	}
+	if ( (fstat(fd2, &fd_stat[1])) == -1)
+	{
+		perror("fstat() for fd2");
+		return -EXIT_FAILURE;
 	}
 
-	struct stat fd2_stat = {};
-	if ( (fstat(fd2, &fd2_stat)) == -1)
-	{
-		perror("stat for fd2");
-		return -1;
-	}
+	// If files are already different, just quit
+	if (fd_stat[0].st_ino != fd_stat[1].st_ino)
+		return FALSE;
 
+	struct statvfs vfs_info[2] = {};
+    if ( fstatvfs(fd1, &vfs_info[0]) == -1 )
+    {
+        perror("fstatvfs() for fd1");
+        return -EXIT_FAILURE;
+    }
+    if ( fstatvfs(fd2, &vfs_info[1]) == -1 )
+    {
+        perror("fstatvfs() for fd2");
+        return -EXIT_FAILURE;
+    }
 
+    // If files are in different filesystems
+    if (vfs_info[0].f_fsid != vfs_info[1].f_fsid)
+    	return FALSE;
+
+    return TRUE;
 }
+
+
